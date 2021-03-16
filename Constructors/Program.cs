9770c7adbe3b8ba2c798a7709a6118eadf8c0198
Galip <Galip@DESKTOP-HRK9HK1>
2021-03-16 18:46:37 +0300

    Generic listleri öğrendik ve kendi generic list mizi oluşturduk. Constroctur yapısınıda burada gördük

---
+++ Constructors/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        //class yapulandırıldığında çalışan uygulamadır.
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer{Id =1,FirstName ="Galip", LastName = "Doğan",City = "İzmir"};
+
+            Customer customer2 = new Customer(2, "Galip", "Doğan", "İzmir");
+
+            
+        }
+
+       static void Method(int Id,string FirstName,string LastName,string City)
+        {
+
+        }
+    }
+
+    class Customer
+    {
+        public Customer(int id, string firstName, string lastName, string city)  // İstersek bu şekilde de override ederek yazabiliriz.
+        {
+            // Bu kısımda constructor çalışacağından parametreleri eşitlememiz gerekiyor.
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+
+        public Customer() //Default Constroctor. Bunu yazmasak bile bu blok oluşur.Class ile aynı isimle olandır. 
+        {
+            
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
